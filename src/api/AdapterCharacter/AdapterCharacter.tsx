e0d192a9a2283e9f2c6da9d784e50004e5d9fe1c
Dc Dev Rep <69812733+DcDevRep@users.noreply.github.com>
2022-09-30 16:47:20 -0500

    act

--- src/api/AdapterCharacter/AdapterCharacter.tsx
+++ src/api/AdapterCharacter/AdapterCharacter.tsx
@@ -1,7 +1,6 @@
 import { useNotification } from '@/context';
-import React from 'react';
 import { characters } from '../endPoint';
-import useState from 'react';
+import { useState } from 'react';
 export interface AdapterCharacterInterface { }
 
 const AdapterCharacter: React.FC<AdapterCharacterInterface> = () => {
@@ -26,30 +25,3 @@ const AdapterCharacter: React.FC<AdapterCharacterInterface> = () => {
 export default AdapterCharacter;
 
 //Data de respuesta -> 20 datos por pag -> 1 de 42.
-const datos = [
-	{
-		created: "2017-11-04T22:39:48.055Z",
-		episode: {
-			0: 'https://rickandmortyapi.com/api/episode/10',
-			1: 'https://rickandmortyapi.com/api/episode/22'
-		},
-		gender: "Male",
-		id: 21,
-		image: "https://rickandmortyapi.com/api/character/avatar/21.jpeg",
-		location:
-		{
-			name: 'Citadel of Ricks',
-			url: 'https://rickandmortyapi.com/api/location/3'
-		},
-		name: "Aqua Morty",
-		origin:
-		{
-			name: 'unknown',
-			url: ''
-		},
-		species: "Humanoid",
-		status: "unknown",
-		type: "Fish-Person",
-		url: "https://rickandmortyapi.com/api/character/21"
-	}
-]
